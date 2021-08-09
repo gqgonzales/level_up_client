4b7a5f9d4e59f003ddde043e9cd7fcfe891afcd4
Gabe Gonzales <gq.gonzales1@gmail.com>
2021-08-09 13:49:32 -0500

    Ch 11 Complete, logged in user can join events

--- src/components/event/EventList.js
+++ src/components/event/EventList.js
@@ -1,47 +1,46 @@
 import React, { useContext, useEffect } from "react";
-import { EventContext } from "../event/EventProvider.js";
+import { EventContext } from "./EventProvider.js";
 import { useHistory } from "react-router-dom";
+import "./events.css";
 
-export const EventList = (props) => {
-  const { events, getEvents } = useContext(EventContext);
+export const EventList = () => {
   const history = useHistory();
+  const { events, getEvents, joinEvent } = useContext(EventContext);
 
   useEffect(() => {
     getEvents();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
-    <>
-      <button
-        className="btn btn-2 btn-sep icon-create"
-        onClick={() => {
-          history.push({ pathname: "/events/new" });
-        }}
-      >
-        Host a New Event
-      </button>
-      <article className="events">
-        <header className="events__header">
-          <h1>Level Up Game Events</h1>
-        </header>
-        {events.map((event) => {
-          return (
-            <section key={event.id} className="registration">
-              <div className="registration__game">{event.game.name}</div>
-              <div>{event.description}</div>
-              <div>
-                {new Date(event.date).toLocaleDateString("en-US", {
-                  weekday: "long",
-                  year: "numeric",
-                  month: "long",
-                  day: "numeric",
-                })}
-                @ {event.time}
-              </div>
-            </section>
-          );
-        })}
-      </article>
-    </>
+    <article className="events">
+      <header className="events__header">
+        <h1>Level Up Game Events</h1>
+        <button
+          className="btn btn-2 btn-sep icon-create"
+          onClick={() => {
+            history.push({ pathname: "/events/new" });
+          }}
+        >
+          Host a New Event
+        </button>
+      </header>
+      {events.map((event) => {
+        // const attending = profile.events.some((evt) => evt.id === event.id);
+        return (
+          <section key={event.id} className="registration">
+            <div className="registration__game">{event.title}</div>
+            <div>
+              {event.description}. We'll be playing {event.game.name}!
+            </div>
+            <div>
+              {event.date} @ {event.time}
+            </div>
+            <button className="btn btn-2" onClick={() => joinEvent(event.id)}>
+              Join
+            </button>
+          </section>
+        );
+      })}
+    </article>
   );
 };
